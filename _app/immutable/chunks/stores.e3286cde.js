97a3823b298fa7360b911ffb183f6384bb8d299a
velociraptor98 <16257541+velociraptor98@users.noreply.github.com>
2023-08-23 22:48:06 +0000

    Deploying to gh-pages from @ velociraptor98/portfolio-svelte@8e5abfe603a41bcedf954889f59d0e7c948e32ed 🚀

---
+++ _app/immutable/chunks/stores.e3286cde.js
@@ -0,0 +1,1 @@
+import"./index.61793a16.js";import{s as e}from"./singletons.77f84717.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
