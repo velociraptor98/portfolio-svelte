2e9c34a2da77fc9de9db704eff81bedd63ee2f16
velociraptor98 <16257541+velociraptor98@users.noreply.github.com>
2023-08-23 22:17:21 +0000

    Deploying to gh-pages from @ velociraptor98/portfolio-svelte@bf0c2e6f64743e8044753eba9ae8fb445dc1a77f 🚀

---
+++ _app/immutable/chunks/paths.db6f4afd.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_oqlr0c)==null?void 0:s.base)??"/slick-portfolio-svelte";var e;const t=((e=globalThis.__sveltekit_oqlr0c)==null?void 0:e.assets)??l;export{t as a,l as b};
